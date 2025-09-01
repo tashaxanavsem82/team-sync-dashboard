2d4f14bd662eb2d03cfcfde11678413cf3f58fd8
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-01 10:54:59 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Team Sync Dashboard</h1>
+        <p>Welcome to the Team Sync Dashboard!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
